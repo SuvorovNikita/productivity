a9b5ea69f79d371bf2a4cad5c95f0594ee0643df
NS <nikit.suvorov@gmail.com>
2023-03-17 14:26:44 +0300

    fixed hedeader animation

--- src/components/MobileHeader.jsx
+++ src/components/MobileHeader.jsx
@@ -21,62 +21,58 @@ const MobileHeader = () => {
                                 <img src={logo} alt="logo"/>
                             </div>
                             <div className="c-mobile-header__toggle" onClick={toggleMenu}>
-                                <span className="c-icon c-icon--menu">open</span>
+                                <span className="c-icon c-icon--menu">{isOpen ? 'close' : 'open'}</span>
                             </div>
                         </div>
-                        {isOpen && (
                             <div className={`c-mobile-header__body ${isOpen ? "is-open" : ""}`}>
-                                <div className="c-mobile-header__inner-body">
-                                    <div className="container">
-                                        <div className="c-mobile-header__flex">
-                                            <div className="c-mobile-header__navigation">
-                                                <div className="c-mobile-header__item">
-                                                    <a href="" className="c-mobile-header__link">Home</a>
-                                                </div>
-                                                <div className="c-mobile-header__item">
-                                                    <a href="" className="c-mobile-header__link">Blog</a>
-                                                </div>
-                                                <div className="c-mobile-header__item">
-                                                    <a href="" className="c-mobile-header__link">Features</a>
-                                                </div>
-                                                <div className="c-mobile-header__item">
-                                                    <a href="" className="c-mobile-header__link">Pricing</a>
-                                                </div>
-                                                <div className="c-mobile-header-header__item">
-                                                    <a href="" className="c-mobile-header__link">Documentation</a>
-                                                </div>
+                                <div className="container">
+                                    <div className="c-mobile-header__flex">
+                                        <div className="c-mobile-header__navigation">
+                                            <div className="c-mobile-header__item">
+                                                <a href="" className="c-mobile-header__link">Home</a>
                                             </div>
-                                            <div className="c-mobile-header__socials">
-                                                <div className="c-mobile-header__social">
-                                                    <a className="c-mobile-header__social-link" href="">
-                                                        <img src={facebook} alt="facebook"/>
-                                                    </a>
-                                                </div>
-                                                <div className="c-mobile-header__social">
-                                                    <a className="c-mobile-header__social-link" href="">
-                                                        <img src={telegram} alt="telegram"/>
-                                                    </a>
-                                                </div>
-                                                <div className="c-mobile-header__social">
-                                                    <a className="c-mobile-header__social-link is-active" href="">
-                                                        <img src={twitter} alt="twitter"/>
-                                                    </a>
-                                                </div>
-                                                <div className="c-mobile-header__social">
-                                                    <a className="c-mobile-header__social-link" href="">
-                                                        <img src={medium} alt="medium"/>
-                                                    </a>
-                                                </div>
+                                            <div className="c-mobile-header__item">
+                                                <a href="" className="c-mobile-header__link">Blog</a>
                                             </div>
-                                            <div className="c-mobile-header__action">
-                                                <a className="c-button c-button--base c-button--base-blue c-button--base-small">Get
-                                                    started</a>
+                                            <div className="c-mobile-header__item">
+                                                <a href="" className="c-mobile-header__link">Features</a>
                                             </div>
+                                            <div className="c-mobile-header__item">
+                                                <a href="" className="c-mobile-header__link">Pricing</a>
+                                            </div>
+                                            <div className="c-mobile-header-header__item">
+                                                <a href="" className="c-mobile-header__link">Documentation</a>
+                                            </div>
+                                        </div>
+                                        <div className="c-mobile-header__socials">
+                                            <div className="c-mobile-header__social">
+                                                <a className="c-mobile-header__social-link" href="">
+                                                    <img src={facebook} alt="facebook"/>
+                                                </a>
+                                            </div>
+                                            <div className="c-mobile-header__social">
+                                                <a className="c-mobile-header__social-link" href="">
+                                                    <img src={telegram} alt="telegram"/>
+                                                </a>
+                                            </div>
+                                            <div className="c-mobile-header__social">
+                                                <a className="c-mobile-header__social-link is-active" href="">
+                                                    <img src={twitter} alt="twitter"/>
+                                                </a>
+                                            </div>
+                                            <div className="c-mobile-header__social">
+                                                <a className="c-mobile-header__social-link" href="">
+                                                    <img src={medium} alt="medium"/>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div className="c-mobile-header__action">
+                                            <a className="c-button c-button--base c-button--base-blue c-button--base-small">Get
+                                                started</a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        )}
                     </div>
                 </div>
             </section>
