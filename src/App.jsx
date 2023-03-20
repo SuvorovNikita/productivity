767c381856d51b651c587e41673a9d55d1f8c3d0
NS <nikit.suvorov@gmail.com>
2023-03-20 15:16:02 +0300

    +

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import Header from "./components/Header.jsx";
 import facebook from "./assets/img/social/facebook.svg";
 import telegram from "./assets/img/social/telegramm.svg";
 import twitter from "./assets/img/social/twitter.svg";
 import medium from "./assets/img/social/medium.svg";
+import bitbucket from "./assets/img/icon/bitbucket.svg";
+import apple from "./assets/img/icon/apple-watch.svg";
+import facebook1 from "./assets/img/icon/facebook.svg";
+import atlassian from "./assets/img/icon/atlassian.svg";
+import audi from "./assets/img/icon/audi.svg";
+
+
+import Header from "./components/Header.jsx";
 import Content from "./components/Content.jsx";
 import Customers from "./components/Customers.jsx";
 
@@ -23,12 +30,20 @@ function App() {
         {id: 4, alt: 'medium', src: medium, link: "#"},
     ]
 
+    const company = [
+        { img: bitbucket, alt: "bitbucket" },
+        { img: apple, alt: "apple" },
+        { img: facebook1, alt: "facebook" },
+        { img: atlassian, alt: "atlassian" },
+        { img: audi, alt: "audi" },
+    ];
+
     return (
         <>
             <Header menu={menu} social={social}/>
             <main className="с-main">
                 <Content/>
-                <Customers/>
+                <Customers company={company}/>
             </main>
         </>)
 }
--- src/components/Customers.jsx
+++ src/components/Customers.jsx
@@ -1,34 +1,19 @@
-import bitbucket from "./../assets/img/icon/bitbucket.svg"
-import apple from "./../assets/img/icon/apple-watch.svg"
-import facebook from "./../assets/img/icon/facebook.svg"
-import atlassian from "./../assets/img/icon/atlassian.svg"
-import audi from "./../assets/img/icon/audi.svg"
 
 
-const Customers = () => {
+const Customers = ({company}) => {
     return (
         <>
             <section className="c-section c-section--base">
                 <div className="container">
                     <div className="c-section__inner">
                         <div className="c-section__body">
-                            <div className="c-section__item">
-                                <img src={bitbucket} alt="bitbucket"/>
-                            </div>
-                            <div className="c-section__item">
-                                <img src={apple} alt="apple"/>
-                            </div>
-                            <div className="c-section__item">
-                                <img src={facebook} alt="facebook"/>
-                            </div>
-                            <div className="c-section__item">
-                                <img src={atlassian} alt="atlassian"/>
-                            </div>
-                            <div className="c-section__item">
-                                <img src={audi} alt="audi"/>
-                            </div>
+                            {company.map((obj, i) => (
+                                <div className="c-section__item" key={i}>
+                                    <img src={obj.img} alt={obj.alt}/>
+                                </div>
+                            ))}
                         </div>
-                        </div>
+                    </div>
                 </div>
             </section>
         </>
